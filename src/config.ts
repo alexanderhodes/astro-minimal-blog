03ffbe3c4873cdc88916a0338ed63e1679857137
Alexander Hodes <alexander.hodes@live.com>
2022-08-26 08:42:44 +0200

    feat: added missing sub title

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Awesome Blog";
+export const SITE_SUB_TITLE = "That's what I'm doing in my freetime.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
