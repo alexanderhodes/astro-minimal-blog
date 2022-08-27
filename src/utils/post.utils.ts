0951159bf274e5b7175c6dd75953edc888300218
Alexander Hodes <alexander.hodes@live.com>
2022-08-27 10:09:25 +0200

    refactor: refactored posts sorting

--- src/utils/post.utils.ts
+++ src/utils/post.utils.ts
@@ -1,20 +1,18 @@
 import type { Post } from "../types/post.type";
 
 export const mapPosts = (posts: Record<string, any>[]): Post[] => {
-  const mappedPosts: Post[] = posts.map((v) => ({
-    url: v["url"],
-    frontmatter: v["frontmatter"],
-  }));
+  const mappedPosts: Post[] = posts
+    .map((v) => ({
+      url: v["url"],
+      frontmatter: v["frontmatter"],
+    }))
+    .sort(
+      (a, b) =>
+        new Date(b.frontmatter.pubDate).valueOf() -
+        new Date(a.frontmatter.pubDate).valueOf()
+    );
 
   return mappedPosts;
-};
-
-export const sortPosts = (posts: Post[]) => {
-  return posts.sort(
-    (a, b) =>
-      new Date(b.frontmatter.pubDate).valueOf() -
-      new Date(a.frontmatter.pubDate).valueOf()
-  );
 };
 
 export const findTags = (posts: Post[]): string[] => {
