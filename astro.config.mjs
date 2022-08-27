407326d8dd0b7851bb4c895d5ef0c6668c11f47c
Alexander Hodes <alexander.hodes@live.com>
2022-08-27 10:50:04 +0200

    feat: added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  site: "https://sunny-arithmetic-1e866e.netlify.app",
+  integrations: [mdx(), sitemap(), tailwind(), compress({
+    css: true,
+    html: false,
+    js: true,
+    img: false,
+    svg: false,
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Awesome Blog";
 export const SITE_SUB_TITLE = "That's what I'm doing in my freetime.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_URL = "https://sunny-arithmetic-1e866e.netlify.app";
